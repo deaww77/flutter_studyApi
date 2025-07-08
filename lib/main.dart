aef6b797c4ba43f1597f93c0f4c7ea5ee69cc4e0
Chanon Roobkhum <126137481+deaww77@users.noreply.github.com>
2025-07-08 23:41:18 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //start hert
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
